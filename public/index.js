ceed7508e06f6b363d80b543713bd2490792b17c
jamesgrubb <jamesgrubb@mac.com>
2017-11-26 20:02:39 +0000

    working locally

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ function setup(){
 createCanvas(600,600)
 background('#FFE624')
 noStroke()
-socket = io.connect('http://localhost:3000')
+socket = io.connect()
 socket.on('mouse' , newDrawing);
 }
 
