60cca1eef4f421342412e316f80108c73edf6fc0
jamesgrubb <jamesgrubb@mac.com>
2017-11-28 22:03:53 +0000

    Updated with screen

--- public/index.js
+++ public/index.js
@@ -1,8 +1,23 @@
+var fixed = document.querySelector('body');
 
+fixed.addEventListener('touchmove', function(e) {
+
+        e.preventDefault();
+
+}, false);
+
+
+let bg;
+let y = 0;
+var img;
+function preload() {
+  img = loadImage("images/wall.jpg");
+}
 var socket;
 function setup(){
-createCanvas(600,600)
-background('#FFE624')
+createCanvas(960,700)
+background(img)
+
 noStroke()
 socket = io.connect()
 socket.on('mouse' , newDrawing);
@@ -23,7 +38,12 @@ function mouseDragged(){
 }
 
 function draw(){
+
     if (mouseIsPressed){
         mouseDragged();
     }
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth,windowHeight)
 }
